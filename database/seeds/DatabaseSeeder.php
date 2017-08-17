53e45a4b5b04dcdaaa5f22da3b3f5c65a6d1c6ac
Kelly Lampotang <klampotang@gmail.com>
2017-08-16 22:46:00 -0400

    php sux

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder {
+/**
+* Run the database seeds.
+*
+* @return void
+*/
+  public function run()
+  {
+    Eloquent::unguard();
+    $this->call('UserTableSeeder');
+  }
+}
+
+class UserTableSeeder extends Seeder {
+  public function run()
+  {
+  	DB::table('users')->delete();
+    // make sure to update this code with your actual e-mail and phone number
+    User::create(array('email' => 'hi@example.com','phone_number' => '+15555555555'));
+  }
+}
