516af6b00154584e5c2bea479d07df8d1fc66ba2
Kelly Lampotang <klampotang@gmail.com>
2017-08-16 23:04:49 -0400

    added routess

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -69,5 +69,8 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+
+        Route::post('user/validate/', 'UserController@validate');
+        Route::post('user/auth/', 'UserController@auth');
     }
 }
